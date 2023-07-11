4347ae48e408748c81bca3441d6b3705105bd745
Ilya Mironov <mironov.ilya1@icloud.com>
2023-07-11 23:57:57 +0300

    Fixed bug with generating new model Added data for generating new model

--- v0.1/Ru/csvReader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def readColumn(file, column, clean):
-    data = pd.read_csv(file)
-    column_data = data[data.columns[column]]
-    if clean:
-        column_data = column_data.str.lower().replace('[\.,1234567890!?()\-\$%"\']', '', regex=True)
-    return column_data.tolist()
